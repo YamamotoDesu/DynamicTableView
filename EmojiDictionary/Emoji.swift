babcf95a124b6931161a317a0a92516042df146f
yamamoto <kyoyamamoto1202@gmail.com>
2021-04-22 09:18:45 +0900

    Created reused emoji cells

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,22 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by 山本響 on 2021/04/21.
+//
+
+import Foundation
+
+class Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+    init(symbol: String, name: String, description: String, usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}
---
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  EmojiTableViewController.swift
+//  EmojiDictionary
+//
+//  Created by 山本響 on 2021/04/21.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+    
+    var emojis: [Emoji] = [
+        Emoji(symbol: "😀", name: "Grinning Face",
+              description: "A typical smiley face.", usage: "happiness"),
+        Emoji(symbol: "😕", name: "Confused Face",
+              description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
+        Emoji(symbol: "😍", name: "Heart Eyes",
+              description: "A smiley face with hearts for eyes.",
+              usage: "love of something; attractive"),
+        Emoji(symbol: "👮", name: "Police Officer",
+              description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"),
+        Emoji(symbol: "🐢", name: "Turtle", description:
+                "A cute turtle.", usage: "Something slow"),
+        Emoji(symbol: "🐘", name: "Elephant", description:
+                "A gray elephant.", usage: "good memory"),
+        Emoji(symbol: "🍝", name: "Spaghetti",
+              description: "A plate of spaghetti.", usage: "spaghetti"),
+        Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
+        Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
+        Emoji(symbol: "📚", name: "Stack of Books",
+              description: "Three colored books stacked on each other.",
+              usage: "homework, studying"),
+        Emoji(symbol: "💔", name: "Broken Heart",
+              description: "A red, broken heart.", usage: "extreme sadness"), Emoji(symbol: "💤", name: "Snore",
+                                                                                    description:
+                                                                                        "Three blue \'z\'s.", usage: "tired, sleepiness"),
+        Emoji(symbol: "🏁", name: "Checkered Flag",
+              description: "A black-and-white checkered flag.", usage:
+                "completion")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+       return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return emojis.count
+        } else {
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
+        let emoji = emojis[indexPath.row]
+        cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
+        cell.detailTextLabel?.text = emoji.description
+        return cell
+    }
+
+}
